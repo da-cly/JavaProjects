0cd58cf819d66b7a8003ed60c46811499898140b
M01 <cly3@ivytech.edu>
2019-09-13 21:54:11 -0400

    1st Commit

--- M01/Exercise 2.3/src/Main.java
+++ M01/Exercise 2.3/src/Main.java
@@ -1,2 +1,16 @@
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter the value for a feet:");
+        double number = input.nextDouble();
+
+        double result = number*0.305;
+        System.out.println(number + " feet is " + result + " meters");
+
+    }
 }
+
+
