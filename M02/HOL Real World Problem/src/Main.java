62a09ad06ac57cd319b3cf350a36e3ac24fc1eb0
M01 <cly3@ivytech.edu>
2019-09-20 15:24:10 -0400

    1st Commit

--- M02/HOL Real World Problem/src/Main.java
+++ M02/HOL Real World Problem/src/Main.java
@@ -7,7 +7,7 @@ public class Main {
         String firstName, lastName, addressLine1, city, state, zip, zipExt, amtOwned, amtPay, datePay;
         zipExt = "1234";
 
-        System.out.println("Please enter your last name: ");
+        System.out.println("Please enter your first name: ");
         firstName = inputPatient.nextLine();
 
         System.out.println("Please enter your last name: ");
@@ -35,11 +35,11 @@ public class Main {
         datePay = inputPatient.nextLine();
 
         System.out.println("\t\t\t\t\t\t\t\t\t\t XYZ Community Hospital");
-        System.out.println("====================================================================================================================");
-        System.out.println("Name \t\t\t\t\t\t\t\t Address \t\t\t\t\t\t\t\t Payment Information");
-        System.out.println("Last  First  Address Line 1 \t\t City\t\t State\t Zip \t\t\t Amount Owed\t Payment Amt.\t Payment Date");
+        System.out.println("========================================================================================================================");
+        System.out.println("\t\t\t Name \t\t\t\t\t\t\t Address \t\t\t\t\t\t\t\t Payment Information");
+        System.out.println("Last  First  Address Line 1\t\t\t City\t\t State\t Zip \t\t\t Amount Owed\t Payment Amt.\t Payment Date");
         System.out.println("Doe\t John\t 1234 Test Drive\t\t AnyCity\t IN\t 12345-1234 \t\t $2000.00\t\t $20.00 \t\t 10/02/2017");
-        System.out.println(firstName + "\t" + lastName + "\t" + addressLine1 + "\t\t" + city + "\t" + state + "\t" + zip + "-" + zipExt + "\t\t\t\t$" + amtOwned + "\t\t$" + amtPay + "\t\t" + datePay);
+        System.out.println(lastName + "\t" + firstName + "\t" + addressLine1 + "\t" + city + "\t" + state + "\t" + zip + "-" + zipExt + "\t\t\t$" + amtOwned + "\t\t\t$" + amtPay + "\t\t\t" + datePay);
 
     }
 }
